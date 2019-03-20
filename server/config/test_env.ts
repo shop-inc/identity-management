16c39ddd19a8d022bf23aadb3969924b25b4b013
Ian Duncan <ianduncan08@gmail.com>
2019-03-20 11:24:22 +0300

    ch(continuous-integration): Configure Circle CI

--- server/config/test_env.ts
+++ server/config/test_env.ts
@@ -7,14 +7,20 @@ const requiredVariables: string[] = [...r];
 // Define optional environment variables and their defaults for development
 const optionalVariables = {
   GRAPH_DB_USER: 'neo4j',
-  GRAPH_DB_PASSWORD: 'admin',
+  GRAPH_DB_PASSWORD: 'neo4j',
   GRAPH_DB_PROTOCOL: 'bolt',
   GRAPH_DB_HOST: 'localhost',
   GRAPH_DB_NAME: 'shopinc_dev',
   GRPC_SERVER_HOST: '127.0.0.1',
   GOOGLE_CLIENT_ID: 'our google client id',
   ADMINISTRATOR_EMAIL: 'admin@shop-inc.com',
-  PROTOBUF_USER: 'protobuf-user',
+  ADMINISTRATOR_NAME: 'Test Admin',
+  PROTOBUF_USER: 'shop-inc',
+  SECRET_KEY: 'secret key',
+  FRONTEND_URL: 'https://url.frontend',
+  MAILGUN_DOMAIN: 'mailgun',
+  MAILGUN_API_KEY: 'mailgun',
+
   ...generalOptionalVariables,
 };
 
