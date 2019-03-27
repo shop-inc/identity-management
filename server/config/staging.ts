2af44f4f01d3738b6036d7b39d44da2e7a7ee302
davenjeru <dave.njeru@andela.com>
2019-03-27 12:44:09 +0300

    ch(authentication): add dockerfile for staging  - add default env variables for staging  - create dockerfile for staging  - add staging scripts in package.json  - add k8s folder.
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- server/config/staging.ts
+++ server/config/staging.ts
@@ -1,3 +1,17 @@
 import getEnv from './utils';
+import generalOptionalVariables from './utils/optionalVariables';
+import r from './utils/requiredVariables';
 
-export default () => getEnv();
+// Define the required variables
+const requiredVariables: string[] = [...r];
+// Define optional environment variables and their defaults for development
+const optionalVariables = {
+  GRAPH_DB_PROTOCOL: 'bolt',
+  GRAPH_DB_NAME: 'shopinc',
+  GRPC_SERVER_HOST: '0.0.0.0',
+  FRONTEND_URL: 'https://frontend.url',
+  PROTOBUF_USER: 'shop-inc',
+  ...generalOptionalVariables,
+};
+
+export default () => getEnv(requiredVariables, optionalVariables);
